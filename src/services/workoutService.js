f1cec0360b06c25e140c0563c09259822aac0ec0
Clay Carney <claycarney7@gmail.com>
2023-02-15 17:21:05 -0500

    Finish setting up error code handling

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -3,46 +3,62 @@ const Workout = require('../database/Workout');
 
 /*---------------- /api/v1/workouts -----------------*/
 
-  const getAllWorkouts = () => {
+const getAllWorkouts = () => {
+  try {
     const allWorkouts = Workout.getAllWorkouts();
     return allWorkouts;
-  };
+  } catch (error) {
+    throw error;
+  }
+};
   
-  const createNewWorkout = (newWorkout) => {
-    const workoutToInsert = {
-      ...newWorkout,
-      id: uuid(),
-      createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
-      updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" })
-    };
-    try {
-      const createdWorkout = Workout.createNewWorkout(workoutToInsert);
-      return createdWorkout;
-    } catch (error) {
-      throw error;
-    }
+const createNewWorkout = (newWorkout) => {
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" })
   };
+  try {
+    const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+    return createdWorkout;
+  } catch (error) {
+    throw error;
+  }
+};
 
 /*---------------- /api/v1/workouts/:workoutId -----------------*/
   
-  const getOneWorkout = (workoutId) => {
+const getOneWorkout = (workoutId) => {
+  try {
     const workout = Workout.getOneWorkout(workoutId);
     return workout;
-  };
+  } catch (error) {
+    throw error;
+  }
+};
   
-  const updateOneWorkout = (workoutId, changes) => {
+const updateOneWorkout = (workoutId, changes) => {
+  try {
     const updatedWorkout = Workout.updateOneWorkout(workoutId, changes);
     return updatedWorkout;
-  };
+  } catch (error) {
+    throw error;
+  }
+};
   
-  const deleteOneWorkout = (workoutId) => {
+const deleteOneWorkout = (workoutId) => {
+  try {
     Workout.deleteOneWorkout(workoutId);
-  };
+  } catch (error) {
+    throw error;
+  }
+};
   
-  module.exports = {
-    getAllWorkouts,
-    createNewWorkout,
-    getOneWorkout,
-    updateOneWorkout,
-    deleteOneWorkout,
-  };
+module.exports = {
+  getAllWorkouts,
+  createNewWorkout,
+  getOneWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
