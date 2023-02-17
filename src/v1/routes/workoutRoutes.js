c67895a5d875509d3ffcefa49dc364922f908655
Clay Carney <claycarney7@gmail.com>
2023-02-17 18:02:12 -0500

    add caching to all routes

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
+
 const router = express.Router();
 
 /*---------------- /api/v1/workouts -----------------*/
